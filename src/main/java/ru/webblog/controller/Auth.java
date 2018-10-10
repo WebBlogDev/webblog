50d403a013f37eccc82bf00247ab990ec51b195b
DESKTOP-774KIG8\Антон Владимирович <anton.unixxx@gmail.com>
2018-10-10 22:56:21 +0500

    Вёрстка логин-формы,добавлен контроллер для rest-авторизации

---
+++ src/main/java/ru/webblog/controller/Auth.java
@@ -0,0 +1,16 @@
+package ru.webblog.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class Auth {
+
+	@RequestMapping(method = RequestMethod.POST,value="doLogin/")
+	public String doLogin(){
+
+		return "{}";
+	}
+}
